ff075a8dbe0ac825a1ef6e284065572a4067e5e7
Moises Icaza <icazamartinez@gmail.com>
2020-04-11 22:30:21 -0500

    Added parameter type to function

--- src/helpers.php
+++ src/helpers.php
@@ -7,7 +7,7 @@ if ( !function_exists( 'is_base64' ) ) {
      * @param string $value String to be evaluated
      * @return bool
      */
-    function is_base64( $value ) {
+    function is_base64( string $value ) {
         return base64_encode( base64_decode( $value, true) ) === $value;
     }
 }
